d0bedc486cd310b3165d3b3f3ef3fe9ca08fdaa0
usefsdd <119659668+usefsdd@users.noreply.github.com>
2022-12-02 17:16:14 +0330

    a ridiculous change

--- test.c
+++ test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main()
 {
+    //i want to change my project but i don't know what can i do so this is a change
     int x;
     scanf("%d",&x);
     for (int i = 1; i <= x; i++)
