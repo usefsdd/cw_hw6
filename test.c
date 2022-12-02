80b353a86fa53f954999d3e57387ab94b0df9b5e
usefsdd <119659668+usefsdd@users.noreply.github.com>
2022-12-02 17:11:22 +0330

    project intialize

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    for (int i = 1; i <= 100; i++)
+    {
+        printf("%d",i);
+        if (i != 100)
+        {
+            printf(",");
+        }
+        
+    }
+    
+}
