c4ac95dd498129a04c7c020c8941d16752ae1e8a
usefsdd <119659668+usefsdd@users.noreply.github.com>
2022-12-02 17:37:00 +0330

    Revert "print salam"
    This reverts commit d8537943b119920f99a501a2215ef9b23330584e.

--- test.c
+++ test.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main()
 {
-
+    //slam man yousef hastem va daram proje ro taqieer midam
     int x;
     scanf("%d",&x);
     for (int i = 1; i <= x; i++)
     {
-        printf("salam%d",i);
+        printf("%d : %d",i,x-i);
         if (i != x)
         {
             printf(",");
